26bae1ee9eb6e9692f80bac1fc74d2581ce7715b
lyupan <lv.steven.pan@gmail.com>
2017-09-23 09:39:58 -0500

    Fix bug

--- src/edu/rice/controller/LoginController.java
+++ src/edu/rice/controller/LoginController.java
@@ -1,16 +1,27 @@
 package edu.rice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import edu.rice.bean.ResultBean;
+import edu.rice.model.User;
+import edu.rice.service.UserService;
+
 @Controller
 public class LoginController {
 	
+	@Autowired
+	public UserService userService;
+	
 	@RequestMapping(value="/login", method = RequestMethod.POST)
 	@ResponseBody
-	public String login(String username, String password) {
-		return username + " " + password;
+	public ResultBean<User> login(String email, String password) {
+		User u = userService.login(email, password);
+		if (u == null)
+			return ResultBean.failure("1001");
+		return ResultBean.success(u);
 	}
 }
