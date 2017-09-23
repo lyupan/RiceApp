e4b2dbd4a85f5bc6c0bbadd4a14c4840a9565779
lyupan <lv.steven.pan@gmail.com>
2017-09-23 03:08:28 -0500

    initial commit

---
+++ src/edu/rice/controller/LoginController.java
@@ -0,0 +1,16 @@
+package edu.rice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	@ResponseBody
+	public String login(String username, String password) {
+		return username + " " + password;
+	}
+}
