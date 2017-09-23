7989983555c2e9ae7516c4d918cb0cfd9b0ad8be
lyupan <lv.steven.pan@gmail.com>
2017-09-23 03:48:50 -0500

    -m "add JJWT jar for JSON Web Token"

---
+++ src/edu/rice/util/Token.java
@@ -0,0 +1,49 @@
+package edu.rice.util;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import io.jsonwebtoken.*;
+import java.util.Date;  
+
+public final class Token {
+	
+	public static final Token Singleton = new Token();
+	
+	private Token() {
+		
+	}
+	
+	//Sample method to construct a JWT
+	public String createJWT(String id, String issuer, String subject, long ttlMillis) {
+	 
+	    //The JWT signature algorithm we will be using to sign the token
+	    SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+	 
+	    long nowMillis = System.currentTimeMillis();
+	    Date now = new Date(nowMillis);
+	 
+	    //We will sign our JWT with our ApiKey secret
+	    byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary("adfdfafdafdafa");
+	    Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+	 
+	    //Let's set the JWT Claims
+	    JwtBuilder builder = Jwts.builder().setId(id)
+	                                .setIssuedAt(now)
+	                                .setSubject(subject)
+	                                .setIssuer(issuer)
+	                                .signWith(signatureAlgorithm, signingKey);
+	 
+	    //if it has been specified, let's add the expiration
+	    if (ttlMillis >= 0) {
+	    long expMillis = nowMillis + ttlMillis;
+	        Date exp = new Date(expMillis);
+	        builder.setExpiration(exp);
+	    }
+	 
+	    //Builds the JWT and serializes it to a compact, URL-safe string
+	    return builder.compact();
+	}
+	
+			
+}
